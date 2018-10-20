5e1a402b69632cbdc0f4b23ccdc1e9b321ef4bb8
Desolve <fp60403@gmail.com>
2018-10-21 04:56:49 +0800

    Finish LeetCode Problem 606 and commit 110's change
    Finish LeetCode Problem:
    606
    Commit 110's change:
    Add return -1 to stop recursive function while meeting unbalanced subtree.
    (When there's a subtree unbalanced, the whole tree will be unbalanced.)

---
+++ 606 Construct String from Binary Tree.java
@@ -0,0 +1,68 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public String tree2str(TreeNode t) {
+        StringBuilder sb = new StringBuilder();
+        preorder(t, sb);
+        return sb.toString();
+    }
+    public void preorder(TreeNode t, StringBuilder sb) {
+        if (t == null) return;
+        sb.append(t.val);
+        if (t.left == null && t.right == null) return;        
+        if (t.left == null) {
+            sb.append("()(");
+            preorder(t.right, sb);
+            sb.append(")");
+        } else {
+            sb.append("(");
+            preorder(t.left, sb);
+            sb.append(")");
+            if (t.right != null) {            
+                sb.append("(");
+                preorder(t.right, sb);
+                sb.append(")");  
+            }
+        }
+    }
+}
+
+/* The above solutoin is simplify from the following solution, both written by myself,
+this one should be easier to understand but slower.
+class Solution {
+    public String tree2str(TreeNode t) {
+        StringBuilder sb = new StringBuilder();
+        preorder(t, sb);
+        return sb.toString();
+    }
+    public void preorder(TreeNode t, StringBuilder sb) {
+        if (t == null) return;
+        sb.append(t.val);
+        if (t.left == null && t.right == null) return;        
+        if (t.left == null) {
+            sb.append("()(");
+            preorder(t.right, sb);
+            sb.append(")");
+            return;
+        }
+        if (t.right == null) {
+            sb.append("(");
+            preorder(t.left, sb);
+            sb.append(")");
+            return;
+        }
+        sb.append("(");
+        preorder(t.left, sb);
+        sb.append(")(");
+        preorder(t.right, sb);
+        sb.append(")");        
+    }
+}
+*/
