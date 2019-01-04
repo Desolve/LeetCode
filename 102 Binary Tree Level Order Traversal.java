dacfcfd2e3d2f87b38b21405cbf53689de02abb4
Desolve <fp60403@gmail.com>
2019-01-04 21:42:17 +0800

    Finish LeetCode Problem 102
    Finish LeetCode Problem:
    102

---
+++ 102 Binary Tree Level Order Traversal.java
@@ -0,0 +1,59 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (root == null) return res;
+        List<Integer> zero = new ArrayList<>();
+        zero.add(root.val);
+        res.add(zero);
+        levelOrder(root.left, res, 1);
+        levelOrder(root.right, res, 1);
+        return res;
+    }
+    public void levelOrder(TreeNode n, List<List<Integer>> res, int level) {
+        if (n == null) return;
+        if (level > res.size() - 1) {
+            List<Integer> lvl = new ArrayList<>();
+            lvl.add(n.val);
+            res.add(lvl);
+        } else {
+            res.get(level).add(n.val);
+        }
+        levelOrder(n.left, res, level + 1);
+        levelOrder(n.right, res, level + 1);
+    }
+}
+
+/* Iterative solution (BFS)
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (root == null)
+            return res;
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        while(!q.isEmpty()) {
+            int levelSize = q.size();
+            List<Integer> currLevel = new ArrayList<>();
+            for(int i = 0; i < levelSize; i++) {
+                TreeNode currNode = q.poll();
+                currLevel.add(currNode.val);
+                if (currNode.left != null)
+                    q.add(currNode.left);
+                if (currNode.right != null)
+                    q.add(currNode.right);
+            }
+            res.add(currLevel);
+        }
+        return res;
+    }
+}
+*/
