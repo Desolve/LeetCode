57888b732179ed3e83804ed0673b359f7c739190
Desolve <fp60403@gmail.com>
2019-01-11 22:19:44 +0800

    Finish LeetCode Problem 114
    Finish LeetCode Problem:
    114

---
+++ 114 Flatten Binary Tree to Linked List.java
@@ -0,0 +1,51 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public void flatten(TreeNode root) {
+        if (root == null) return;
+        Stack<TreeNode> st = new Stack<>();
+        pushStack(st, root);
+        st.pop();
+        while(!st.isEmpty()) {
+            root.left = null;
+            root.right = st.pop();
+            root = root.right;
+        }
+    }
+    public void pushStack(Stack<TreeNode> st, TreeNode curr) {
+        if (curr == null) return;
+        pushStack(st, curr.right);
+        pushStack(st, curr.left);
+        st.push(curr);
+    }
+}
+
+/* faster solution without stack
+class Solution {
+    public void flatten(TreeNode root) {
+        if (root != null) {
+            helper(root);
+        }
+    }
+    public TreeNode helper(TreeNode curr) {
+        if (curr.left == null && curr.right == null) return curr;
+        TreeNode l = curr.left;
+        TreeNode r = curr.right;
+        
+        TreeNode next = (l == null) ? r : l;
+        TreeNode lEnd = (l == null) ? curr : helper(l);
+        TreeNode rEnd = (r == null) ? lEnd : helper(r);
+        lEnd.right = r;
+        curr.left = null;
+        curr.right = next;
+        return rEnd;
+    }
+}
+*/
